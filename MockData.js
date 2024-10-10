4147908a6aa65377ee27088c60d087717b0fcc34
Eleomar Halaman <73923255+kaotikus27@users.noreply.github.com>
2024-10-10 15:19:14 +0800

    Update MockData.js

--- MockData.js
+++ MockData.js
@@ -1,60 +1,62 @@
- {
-      "itemId": 1,
-      "name": "Hannah Gown",
-      "description": "Festive Looks Rust Red Ribbed Velvet Long Sleeve Bodysuit",
-      "size": "S",
-      "color": "Multicolor",
-      "stylenumber": "BROR-WD106",
-      "price": "38",
-      "image": "Rectangle 1r.png",
-      "id": "7c21",
-      "qnty": 1
-    },
-    {
-      "itemId": 2,
-      "name": "Bag Flap",
-      "description": "Chevron Flap Crossbody Bag",
-      "size": "M",
-      "color": "Multicolor",
-      "stylenumber": "BROR-XD121",
-      "price": "5.77",
-      "image": "Rectangle 2r.png",
-      "id": "f6fc",
-      "qnty": 1
-    },
-    {
-      "itemId": 3,
-      "name": "Scarf Fringe",
-      "description": "Manilla Tan Multi Plaid Oversized Fringe Scarf",
-      "size": "L",
-      "color": "Beige",
-      "stylenumber": "BROR-DD102",
-      "price": "29.77",
-      "image": "Rectangle 3r.png",
-      "id": "452a",
-      "qnty": 1
-    },
-    {
-      "itemId": 4,
-      "name": "Sleeve Dress Diamante",
-      "description": "Diamante Puff Sleeve Dress - Black",
-      "size": "M",
-      "color": "Black Gold",
-      "stylenumber": "BROR-KS122",
-      "price": "45.77",
-      "image": "Rectangle 4r.png",
-      "id": "4c1d",
-      "qnty": 1
-    },
-    {
-      "itemId": 5,
-      "name": "Banneth DiaFormal Dress",
-      "description": "Banneth Open Front Formal Dress in Black",
-      "size": "XL",
-      "color": "Black",
-      "stylenumber": "BROR-XX152",
-      "price": "45.77",
-      "image": "Rectangle 5r.png",
-      "id": "5ea9",
-      "qnty": 1
-    }
+[
+  {
+    "itemId": 1,
+    "name": "Hannah Gown",
+    "description": "Festive Looks Rust Red Ribbed Velvet Long Sleeve Bodysuit",
+    "size": "S",
+    "color": "Multicolor",
+    "stylenumber": "BROR-WD106",
+    "price": "38",
+    "image": "Rectangle 1r.png",
+    "id": "7c21",
+    "qnty": 1
+  },
+  {
+    "itemId": 2,
+    "name": "Bag Flap",
+    "description": "Chevron Flap Crossbody Bag",
+    "size": "M",
+    "color": "Multicolor",
+    "stylenumber": "BROR-XD121",
+    "price": "5.77",
+    "image": "Rectangle 2r.png",
+    "id": "f6fc",
+    "qnty": 1
+  },
+  {
+    "itemId": 3,
+    "name": "Scarf Fringe",
+    "description": "Manilla Tan Multi Plaid Oversized Fringe Scarf",
+    "size": "L",
+    "color": "Beige",
+    "stylenumber": "BROR-DD102",
+    "price": "29.77",
+    "image": "Rectangle 3r.png",
+    "id": "452a",
+    "qnty": 1
+  },
+  {
+    "itemId": 4,
+    "name": "Sleeve Dress Diamante",
+    "description": "Diamante Puff Sleeve Dress - Black",
+    "size": "M",
+    "color": "Black Gold",
+    "stylenumber": "BROR-KS122",
+    "price": "45.77",
+    "image": "Rectangle 4r.png",
+    "id": "4c1d",
+    "qnty": 1
+  },
+  {
+    "itemId": 5,
+    "name": "Banneth DiaFormal Dress",
+    "description": "Banneth Open Front Formal Dress in Black",
+    "size": "XL",
+    "color": "Black",
+    "stylenumber": "BROR-XX152",
+    "price": "45.77",
+    "image": "Rectangle 5r.png",
+    "id": "5ea9",
+    "qnty": 1
+  }
+]
